737000b6970e834c81863026fd8850aa0d24fdec
VerCh93 <chlumska.ver@gmail.com>
2024-08-03 10:25:15 +0200

    Update upload

--- Anagram.java
+++ Anagram.java
@@ -1,29 +1,41 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Anagram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in, "Windows-1250");
         System.out.println("Input first word:");
-        String word = scanner.nextLine();
+        String word = scanner.nextLine().toLowerCase().trim();
         System.out.println("Input second word:");
-        String word2 = scanner.nextLine();
+        String word2 = scanner.nextLine().toLowerCase().trim();
 
         if (word.length() != word2.length()) {
             System.out.println("Not an anagram");
+            return;
+        }
+        if (isAnagram(word, word2)) {
+            System.out.println("Anagram!");
+        } else {
+            System.out.println("Not an anagram");
         }
 
-        boolean isAnagram = true;
+    }
+
+    private static boolean isAnagram(String word, String word2) {
+        ArrayList<Character> array1 = new ArrayList<Character>();
+        ArrayList<Character> array2 = new ArrayList<Character>();
 
         for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
+            array1.add(word.charAt(i));
         }
         for (int j = 0; j < word2.length(); j++) {
-            char ch2 = word2.charAt(j);
+            array2.add(word2.charAt(j));
         }
+        Collections.sort(array1);
+        Collections.sort(array2);
 
-
-
-
+        return array1.equals(array2);
     }
 }
